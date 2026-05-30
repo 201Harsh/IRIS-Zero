3ab3c4ed852bd94854b05b4807dff697a3e792a7
Harsh <pandeygautam039@gmail.com>
2026-05-30 19:56:45 +0530

    update IrisZero.tsx

--- src/client/IrisZero.tsx
+++ src/client/IrisZero.tsx
@@ -1,6 +1,16 @@
-
 const IrisZero = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-screen h-screen bg-black">
+        <div className="w-1/4 bg-white"></div>
+        <div className="w-3/4 bg-black flex flex-col items-center justify-center">
+          <div className="w-1/2 h-1/2 border border-white flex items-center justify-center rounded-2xl text-6xl text-white">
+            IRIS Zero
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default IrisZero;
